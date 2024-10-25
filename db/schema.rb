e02d223b0244b48a8b7b3626a55b28e3056618ac
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-26 01:21:09 +0800

    modified role column to integer and added enum role in user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_25_150339) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_25_171018) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_25_150339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "role"
+    t.integer "role", default: 0
     t.string "phone"
     t.integer "coins"
     t.decimal "total_deposit", precision: 10
