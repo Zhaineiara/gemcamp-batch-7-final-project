47b42113935bd59dc3a69c0fc8faadf77edd34e4
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-26 16:11:46 +0800

    added admin account in seeds [feature 1]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'admin123@gmail.com', password: 'admin123', username: 'admin123', role: 'admin', phone: '+639995240842', coins: 0, total_deposit: 0.0, children_members: 0)
