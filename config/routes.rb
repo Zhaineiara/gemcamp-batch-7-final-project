f32a88ff99dc65fea60732e3271cc4bc4b5f59df
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-27 13:24:15 +0800

    added separate directories for admin and client [feature 1]

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'home/index'
+  end
+  namespace :client do
+    get 'home/index'
+  end
   devise_for :users
 
   constraints(ClientDomainConstraint.new) do
     authenticated :user, ->(u) { u.client? } do
-      root 'home#client', as: :client_root
+      root 'client/home#index', as: :client_root
     end
   end
 
   constraints(AdminDomainConstraint.new) do
     authenticated :user, ->(u) { u.admin? } do
-      root 'home#admin', as: :admin_root
+      root 'admin/home#index', as: :admin_root
     end
   end
 
