f14415f6a8145f08a5a77c10c8bb15bb515ad245
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-27 09:53:27 +0800

    added routes for client and admin [feature 1]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'home#index'
+  authenticated :user, ->(u) { u.admin? } do
+    root 'home#admin', as: :admin_root
+  end
+
+  authenticated :user, ->(u) { u.client? } do
+    root 'home#client', as: :client_root
+  end
+
+  unauthenticated do
+    root 'home#index'
+  end
 end
